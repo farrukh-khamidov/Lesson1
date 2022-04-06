00c53be4c61812a50820d7a7fd05f39db1deb3dc
farrukh-khamidov <mrfarrukh.94@gmail.com>
2022-04-06 08:44:19 +0500

    Init commit

---
+++ src/Lesson1.java
@@ -0,0 +1,83 @@
+public class Lesson1 {
+
+    // types 8
+    // butun: byte short int long
+    // haqiqiy: float double
+    // simvol: char
+    // mantiqiy: boolean
+
+    // 4 gb  8 * 1024 * 1024 *1024 * 4
+
+
+
+    public static void main(String[] args) {
+//        long number = 8L * 1024 * 1024 *1024 * 4;
+//        System.out.println(number);
+//
+//        long num = 5645421321_213_213L;
+//        System.out.println(num);
+//
+//        // amallar: + - * / %
+//
+//        byte num2 = 3 + 5;
+//        int num3 = 5-3;
+
+//        int num4 = Integer.MAX_VALUE + 1;
+//        System.out.println(num4);
+//
+//        long num5 = Integer.MAX_VALUE + 1L;
+//        System.out.println(num5);
+
+//        float a = 4 * 5.0F;
+
+//        float a = 6.0F/4;
+//        System.out.println(a);
+//
+//        // qoldiqli qolish
+//
+//        double b = 7.145 % 3;
+//        System.out.println(b);
+        // -128 127
+//        System.out.println(Math.absExact(-2147483648));
+
+//        int num = (int) (50 + 51 * Math.random()); // [0, 1)
+//
+//        System.out.println(num);
+
+        System.out.println(Math.rint(3.49999999999));
+        System.out.println(Math.round(3.49999999999));
+        System.out.println(Math.ceil(3.49999999999));
+        System.out.println(Math.floor(3.49999999999));
+
+        System.out.println();
+        System.out.println( );
+
+        System.out.println(Math.rint(3.89999999999));
+        System.out.println(Math.round(3.89999999999));
+        System.out.println(Math.ceil(3.89999999999));
+        System.out.println(Math.floor(3.89999999999));
+
+        System.out.println();
+        System.out.println("-----------------");
+        System.out.println();
+        System.out.println(Math.rint(-3.49999999999));
+        System.out.println(Math.round(-3.49999999999));
+        System.out.println(Math.ceil(-3.49999999999));
+        System.out.println(Math.floor(-3.49999999999));
+
+        System.out.println();
+        System.out.println( );
+
+        System.out.println(Math.rint(-3.89999999999));
+        System.out.println(Math.round(-3.89999999999));
+        System.out.println(Math.ceil(-3.89999999999));
+        System.out.println(Math.floor(-3.89999999999));
+
+
+
+
+
+
+    }
+
+}
